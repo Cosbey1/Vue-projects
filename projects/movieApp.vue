f96fd0f8f4b8bf8c51babdc4fc42ee61bd5df5bf
CleoBreaux <corey.osbey@gmail.com>
2023-01-23 16:50:24 -0600

    start of movie app through vue

---
+++ projects/movieApp.vue
@@ -0,0 +1,21 @@
+
+    <script setup>
+
+let message = "Hello World!";
+
+</script>
+<!-- HTML -->
+<template>
+
+    <h1 class="welcome">{{ message }}</h1>
+
+</template>
+<!-- CSS -->
+<style scoped>
+    .welcome {
+    text-align: center;
+    color: lightblue;
+    background-color: black;
+}
+
+</style>
